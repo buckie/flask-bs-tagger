5482547c58a29f07ad367a657b7ef0ad9d8162f7
buckie <wjmartino@gmail.com>
2014-12-07 20:02:24 -0500

    changes the layout to get freezer to work
    Signed-off-by: buckie <wjmartino@gmail.com>

--- flask_tagger/templates/tag_form.js
+++ flask_tagger/templates/tag_form.js
@@ -2,7 +2,7 @@ var citynames = new Bloodhound({
   datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
   queryTokenizer: Bloodhound.tokenizers.whitespace,
   prefetch: {
-    url: 'static/assets/citynames.json',
+    url: '/static/assets/citynames.json',
     filter: function(list) {
       return $.map(list, function(cityname) {
         return { name: cityname }; });
